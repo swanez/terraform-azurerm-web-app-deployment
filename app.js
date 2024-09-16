c4882bd80df71a36660824a776519abfafbc2927
Peter Sinda <sindapeter93@gmail.com>
2024-09-16 02:55:20 +0200

    Add initial node.js application config

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
- 
+const http = require('http');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, Heroku and Terraform!');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
