22c5d5799d9be70184bfd8f63164571f32174a50
Peter Sinda <sindapeter93@gmail.com>
2024-09-16 03:48:12 +0200

    Modifications made in Node.js application

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Heroku and Terraform!');
+  res.end('Hello DevOps! Node.js application built in Docker, hosted on Heroku provisioned by Terraform!');
 });
 
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
